5c6e1fabf8616096afe7c06b1306bfdb86623a16
Qiu1996 <betty52332001@gmail.com>
2025-07-20 19:31:57 +0800

    05-randow-password-generatoe 使用Alpine將按鈕間的交互綁定完成

--- 05-random-password-generator/script/all.js
+++ 05-random-password-generator/script/all.js
@@ -4,5 +4,5 @@ import { Generator } from './generator'  //  從 ./generator 導入 Generator
 window.Alpine = Alpine;  
 // 將 Alpine 掛載在全域 window 物件上
 
-Alpine.data("password_generator", Generator);  // 註冊組件
+Alpine.data("generator", Generator);  // 註冊組件
 Alpine.start();  // 啟動 Alpine.js
--- 05-random-password-generator/script/generator.js
+++ 05-random-password-generator/script/generator.js
@@ -1,7 +1,30 @@
 const Generator = () => {
     return{
-        status: 'off',
+        upperToggle: true,
+        lowerToggle: true,
+        symbolToggle: true,
+        numberToggle: true,
+        password: () => {
+            return '123';
+        },
+        uppercase(){
+            this.upperToggle = this.upperToggle ? false : true;
+            console.log(`大寫開關：${this.upperToggle}`);
+        },
+        lowercase(){
+            this.lowerToggle = this.lowerToggle ? false : true;
+            console.log(`小寫開關：${this.lowerToggle}`);
+        },
+        symbol(){
+            this.symbolToggle = this.symbolToggle ? false : true;
+            console.log(`符號開關：${this.symbolToggle}`);
+        },
+        number(){
+            this.numberToggle = this.numberToggle ? false : true;
+            console.log(`數字開關：${this.numberToggle}`);
+        }
     }
 }
+
 
 export { Generator }
