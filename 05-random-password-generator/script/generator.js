e887777f00e9579c1433c281904e27ad7ec6085a
Qiu1996 <betty52332001@gmail.com>
2025-07-20 22:22:35 +0800

    05-隨機密碼 功能完成

--- 05-random-password-generator/script/generator.js
+++ 05-random-password-generator/script/generator.js
@@ -1,11 +1,31 @@
 const Generator = () => {
     return{
+        setLength: 20,
         upperToggle: true,
         lowerToggle: true,
         symbolToggle: true,
         numberToggle: true,
-        password: () => {
-            return '123';
+        upperChars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        lowerChars: 'abcdefghijklmnopqrstuvwxyz',
+        symbolChars: '!@#$%^&*',
+        numberChars: '0123456789',
+        refreshPassword: false,
+        copyStatus: '複製密碼',
+        get getPassword(){
+            this.refreshPassword;
+            let char = '';
+            let str = '';
+            if(this.upperToggle)char += this.upperChars;
+            if(this.lowerToggle)char += this.lowerChars;
+            if(this.symbolToggle)char += this.symbolChars;
+            if(this.numberToggle)char += this.numberChars;
+            
+            for(let i = 0; i < this.setLength; i++){
+                const index = Math.floor(Math.random() * char.length);
+                str += char.charAt(index);
+                // 移除已選擇的字元
+            }
+            return str;
         },
         uppercase(){
             this.upperToggle = this.upperToggle ? false : true;
@@ -22,6 +42,21 @@ const Generator = () => {
         number(){
             this.numberToggle = this.numberToggle ? false : true;
             console.log(`數字開關：${this.numberToggle}`);
+        },
+        refresh(){
+            this.refreshPassword = true;
+            setTimeout(() => {
+                this.refreshPassword = false;
+            }, 1000);
+            console.log('刷新密碼');
+        },
+        copy(){
+            navigator.clipboard.writeText(this.getPassword);
+            this.copyStatus = '複製成功';
+            setTimeout(() => {
+                this.copyStatus = '複製密碼';
+            }, 1000);
+            console.log('複製成功');
         }
     }
 }
