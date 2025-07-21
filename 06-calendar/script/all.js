1828ddbdc9fcc444072f687cc8a34dc262198fc1
Qiu1996 <betty52332001@gmail.com>
2025-07-21 15:31:34 +0800

    05-日曆 互動跟事件，整體完成

--- 06-calendar/script/all.js
+++ 06-calendar/script/all.js
@@ -4,26 +4,27 @@ window.Alpine = Alpine
  
 Alpine.data('CalendarData', () => ({
     today: new Date(),
-    year: null,
-    month: null,
+    year: new Date().getFullYear(),
+    month: new Date().getMonth(),
     firstDayOfWeek: null,
     lastDay: null,
-    totalGrid: 35,
     weeks: ['一', '二', '三', '四', '五', '六', '日'],
-    init(){
-        this.year = this.today.getFullYear();
-        this.month = this.today.getMonth();
-        
+    get title(){
+        let str = `${this.year} 年 ${this.month + 1} 月`;
+        return str;
+    },
+    get days(){
+        return this.getDays();
+    },
+    getDays(){
         let firstDay = new Date(this.year, this.month, 1);
         this.firstDayOfWeek = firstDay.getDay() === 0 ? 7 : firstDay.getDay();
         this.lastDay = new Date(this.year, this.month + 1, 0).getDate();
-    },
-    days: function(){
+        
         let arr = [];
         let day = 0;
-        console.log(this.lastDay);
-        for(let i = 1; i <= this.totalGrid; i++){
-            if(i >= this.firstDayOfWeek && i <= this.lastDay +1){
+        for(let i = 1; day < this.lastDay; i++){
+            if(i >= this.firstDayOfWeek){
                 day += 1;
                 arr.push(day);
             }else{
@@ -32,6 +33,25 @@ Alpine.data('CalendarData', () => ({
         }
         return arr;
     },
+    prev(){
+        this.month -= 1;
+        if(this.month < 0){
+            this.year -= 1;
+            this.month = 11;
+        };
+    },
+    next(){
+        this.month += 1;
+        if(this.month > 11){
+            this.year += 1;
+            this.month = 0;
+        };
+    },
+    backBegin(){
+        this.month = new Date().getMonth();
+        this.year = new Date().getFullYear();
+    }
+
 }))
 
 Alpine.start()
