16ec22d3a25a098023e7108611c9c217e3275ce1
Qiu1996 <betty52332001@gmail.com>
2025-07-21 14:22:11 +0800

    05-日曆 AlpineJS 模版跟資料榜定完成

--- 06-calendar/script/all.js
+++ 06-calendar/script/all.js
@@ -3,7 +3,35 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
  
 Alpine.data('CalendarData', () => ({
-    
+    today: new Date(),
+    year: null,
+    month: null,
+    firstDayOfWeek: null,
+    lastDay: null,
+    totalGrid: 35,
+    weeks: ['一', '二', '三', '四', '五', '六', '日'],
+    init(){
+        this.year = this.today.getFullYear();
+        this.month = this.today.getMonth();
+        
+        let firstDay = new Date(this.year, this.month, 1);
+        this.firstDayOfWeek = firstDay.getDay() === 0 ? 7 : firstDay.getDay();
+        this.lastDay = new Date(this.year, this.month + 1, 0).getDate();
+    },
+    days: function(){
+        let arr = [];
+        let day = 0;
+        console.log(this.lastDay);
+        for(let i = 1; i <= this.totalGrid; i++){
+            if(i >= this.firstDayOfWeek && i <= this.lastDay +1){
+                day += 1;
+                arr.push(day);
+            }else{
+                arr.push('');
+            }
+        }
+        return arr;
+    },
 }))
 
 Alpine.start()
