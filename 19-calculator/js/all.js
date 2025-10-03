bd214989d550d48d7fa562bce6730366e7e42a09
Qiu1996 <betty52332001@gmail.com>
2025-10-03 12:33:17 +0800

    feat: 數字加減功能

--- 19-calculator/js/all.js
+++ 19-calculator/js/all.js
@@ -7,18 +7,32 @@ el_btnWrap.addEventListener("click", e => {
     case "clear":
       el_result.innerText = '0';
       break;
-    case "num":
-      el_result.innerText = addNum(e.target.textContent);
+    case "equal":
+      fn_equal()
+      break;
+    default:
+      fn_default(e.target.textContent);
       break;
     
   }
 })
 
-const addNum = ((text) => {
-  let str = '';
-
-  return (text) => {
-    str += text;
-    return str;
+function fn_default(text){
+  let str = el_result.innerText;
+  el_result.innerText = str === "0" ? "" : str;
+  
+  if(text === "+" || text === "-"){
+    if(str[str.length-1] === "+" || str[str.length-1] === "-"){
+      el_result.innerText = str.slice(0, -1);
+    } 
   }
-})();
+  el_result.innerText += text;
+}
+
+function fn_equal(){
+  try{
+    el_result.innerText = eval(el_result.innerText);
+  }catch{
+    return
+  }
+}
